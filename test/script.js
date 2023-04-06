d1925f1fe7c406d67b9184d5dc126315a982b442
Yaroslav <sych521@gmail.com>
2023-04-06 19:59:32 +0300

    default

---
+++ test/script.js
@@ -0,0 +1,18 @@
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "На березі спокійних вод",
+    author: "Роберт Шеклі",
+    rating: 8.51,
+  },
+];
+
+for (const book of books) {
+  console.log(book.title);
+  console.log(book.author);
+  console.log(book.rating);
+}
