577dddc27228e0a2d2438ea68d828b5e3f9a9c2c
Yaroslav <sych521@gmail.com>
2023-04-10 21:07:57 +0300

    First task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.getElementsByTagName('button')[0];
+const stopButton = document.getElementsByTagName('button')[1];
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let intervalID;
+
+startButton.addEventListener('click', startButtonFunc);
+function startButtonFunc() {
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startButton.disabled = true;
+}
+
+stopButton.addEventListener('click', stopButtonFunc);
+function stopButtonFunc() {
+  clearInterval(intervalID);
+  startButton.disabled = false;
+}
